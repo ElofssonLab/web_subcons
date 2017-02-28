b042b21a03fbe2885a5d0d22f0cfac38c242e3b7
Nanjiang Shu <nanjiang.shu@bils.se>
2017-02-28 15:55:37 +0100

    added the alert_email.txt and the alerting email is set there

--- proj/pred/app/check_web_server.pl
+++ proj/pred/app/check_web_server.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl -w
-# Filename:   check_web_server.pl
+# Filename:  check_web_server.pl
 
-# Description: check whether web-server is accessable and also check the status
+# Description: check whether web server is accessable and also check the status
 #              of the qd_fe.py
 
-# Created 2016-12-07, updated 2016-12-07, Nanjiang Shu
+# Created 2016-12-07, updated 2017-02-28, Nanjiang Shu
 
 use File::Temp;
 
@@ -23,39 +23,25 @@ my @to_email_list = (
 
 my $date = localtime();
 print "Date: $date\n";
-my $url = "http://subcons.bioinfo.se";
+my $url = "";
+my $servername = "SubCons";
 my @urllist = ("http://subcons.bioinfo.se");
 my $target_qd_script_name = "qd_fe.py";
 my $computenodelistfile = "$basedir/static/computenode.txt";
+my $alert_emaillist_file = "$basedir/static/alert_email.txt";
 my $from_email = "nanjiang.shu\@scilifelab.se";
 my $title = "";
 my $output = "";
 
-foreach $url (@urllist){ 
-# first: check if $url is accessable
-    if (!head($url)){
-        $title = "$url un-accessible";
-        $output = "$url un-accessible";
-        foreach my $to_email(@to_email_list) {
-            sendmail($to_email, $from_email, $title, $output);
-        }
-    }
-
-# second: check if qd_topcons2_fe.pl running at pcons1.scilifelab.se frontend
-    my $num_running=`curl $url/cgi-bin/check_qd_fe.cgi 2> /dev/null | html2text | grep  "$target_qd_script_name" | wc -l`;
-    chomp($num_running);
-
-    if ($num_running < 1){
-        $output=`curl $url/cgi-bin/restart_qd_fe.cgi 2>&1 | html2text`;
-        $title = "$target_qd_script_name restarted for $url";
-        foreach my $to_email(@to_email_list) {
-            sendmail($to_email, $from_email, $title, $output);
-        }
-    }
+my @to_email_list = ();
+open(IN, "<", $alert_emaillist_file) or die;
+while(<IN>) {
+    chomp;
+    push @to_email_list, $_;
 }
+close IN;
 
-# third, check if the suq queue is blocked at the compute node and try to clean
-# it if blocked
+
 my %computenodelist = ();
 open(IN, "<", $computenodelistfile) or die;
 while(<IN>) {
@@ -66,15 +52,39 @@ while(<IN>) {
     }
 }
 close IN;
+foreach $url (@urllist){ 
+# First: check if the $url is accessable
+    if (!head($url)){
+        $title = "[$servername] $url un-accessible";
+        $output = "$url un-accessible";
+        foreach my $to_email(@to_email_list) {
+            sendmail($to_email, $from_email, $title, $output);
+        }
+    }
 
+# Second: check if qd is running at the front-end
+    my $num_running=`curl $url/cgi-bin/check_qd_fe.cgi 2> /dev/null | html2text | grep  "$target_qd_script_name" | wc -l`;
+    chomp($num_running);
+
+    if ($num_running < 1){
+        $output=`curl $url/cgi-bin/restart_qd_fe.cgi 2>&1 | html2text`;
+        $title = "[$servername] $target_qd_script_name restarted for $url";
+        foreach my $to_email(@to_email_list) {
+            sendmail($to_email, $from_email, $title, $output);
+        }
+    }
+}
+
+# Third, check if the suq queue is blocked at the compute node and try to clean
+# it if blocked
 foreach (sort keys %computenodelist){
     my $computenode = $_;
     my $max_parallel_job = $computenodelist{$_};
     print "curl http://$computenode/cgi-bin/clean_blocked_suq.cgi 2>&1 | html2text\n";
     $output=`curl http://$computenode/cgi-bin/clean_blocked_suq.cgi 2>&1 | html2text`;
+    `curl http://$computenode/cgi-bin/set_suqntask.cgi?ntask=$max_parallel_job `;
     if ($output =~ /Try to clean the queue/){
-        $title = "Cleaning the queue at $computenode";
-        `curl http://$computenode/cgi-bin/set_suqntask.cgi?ntask=$max_parallel_job `;
+        $title = "[$servername] Cleaning the queue at $computenode";
         foreach my $to_email(@to_email_list) {
             sendmail($to_email, $from_email, $title, $output);
         }
